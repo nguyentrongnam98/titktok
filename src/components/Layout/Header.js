79d01badc5c2954d1aedfaed4d7a466d4ee4d85e
nguyentrongnam98 <ww3ww32@gmail.com>
2022-05-17 16:42:20 +0700

    update code header ui

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '../../assets/images/logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images} alt="logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                        {/* <FontAwesomeIcon icon={faSpinner} className={cx('loading')} /> */}
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
